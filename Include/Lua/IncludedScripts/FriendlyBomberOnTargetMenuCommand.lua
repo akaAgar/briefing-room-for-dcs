e274ca67e68cf60fc2ae0593b96c2c214abc1e05
john681611 <john681611@hotmail.com>
2021-03-14 10:00:39 +0000

    fix radio bomber and clean commands

--- Include/Lua/IncludedScripts/FriendlyBomberOnTargetMenuCommand.lua
+++ Include/Lua/IncludedScripts/FriendlyBomberOnTargetMenuCommand.lua
@@ -1,29 +1,21 @@
-briefingRoom.mission.features.friendlyBomberOnTarget = { }
-briefingRoom.mission.features.friendlyBomberOnTarget.activated = { } -- was the bomber FG activated?
+briefingRoom.mission.features.friendlyBomberOnTarget = { } -- was the bomber FG activated?
 
 -- Activate aircraft and launch bombing run
 function briefingRoom.mission.features.friendlyBomberOnTarget.beginBombingRun(index)
   briefingRoom.radioManager.play("Threats have been suppressed, you may begin your bombing run.", "RadioPilotBeginYourBombingRun")
-
-  local bomberGroup = dcsExtensions.getGroupByID(briefingRoom.mission.featuresUnitGroups.FriendlyBomberOnTarget[index])
+  local bomberGroup = dcsExtensions.getGroupByID(briefingRoom.mission.featuresUnitGroups.FriendlyBomberOnTargetAndTakeOffCommand[index])
   if bomberGroup == nil then return end -- no group (destroyed? missing?), no radio answer
-
-  -- bomber already activated
-  if briefingRoom.mission.features.friendlyBomberOnTarget.activated[index] then
-    briefingRoom.radioManager.play("Negative, bombing mission already in progress.", "RadioOtherPilotAlreadyBombing", briefingRoom.radioManager.getAnswerDelay())
-    return
-  end
 
   briefingRoom.radioManager.play("Affirm. Taking off, on my way to bomb the target location.", "RadioOtherPilotBeginBombing", briefingRoom.radioManager.getAnswerDelay())
   bomberGroup:activate()
-  briefingRoom.mission.features.friendlyBomberOnTarget.activated[index] = true
+  missionCommands.removeItemForCoalition($PLAYERCOALITION$, briefingRoom.f10Menu.bomberCommands[index])
 end
 
 -- Create F10 menu options
+briefingRoom.f10Menu.bomberCommands = {}
 do
   for i,o in ipairs(briefingRoom.mission.objectives) do
-    missionCommands.addCommandForCoalition($PLAYERCOALITION$, "Bomber, take off and begin bombing run", briefingRoom.f10Menu.objectives[i], briefingRoom.mission.features.friendlyBomberOnTarget.beginBombingRun, i)
-    table.insert(briefingRoom.mission.features.friendlyBomberOnTarget.activated, false) -- set all activated indices to false
+    briefingRoom.f10Menu.bomberCommands[i] = missionCommands.addCommandForCoalition($PLAYERCOALITION$, "Bomber, take off and begin bombing run", briefingRoom.f10Menu.objectives[i], briefingRoom.mission.features.friendlyBomberOnTarget.beginBombingRun, i)
   end
 end
 
