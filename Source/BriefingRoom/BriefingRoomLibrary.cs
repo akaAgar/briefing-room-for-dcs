04450a7f744813cfc0d51562ad8daaf21a2913ff
Agar <47314805+akaAgar@users.noreply.github.com>
2021-04-23 10:19:26 +0200

    Created skeleton for the new DBEntry classes and the library entry point

---
+++ Source/BriefingRoom/BriefingRoomLibrary.cs
@@ -0,0 +1,48 @@
+/*
+==========================================================================
+This file is part of Briefing Room for DCS World, a mission
+generator for DCS World, by @akaAgar (https://github.com/akaAgar/briefing-room-for-dcs)
+
+Briefing Room for DCS World is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+Briefing Room for DCS World is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Briefing Room for DCS World. If not, see https://www.gnu.org/licenses/
+==========================================================================
+*/
+
+using System;
+using BriefingRoom.DB;
+using BriefingRoom.Mission;
+using BriefingRoom.Template;
+
+namespace BriefingRoom
+{
+    /// <summary>
+    /// Main class for the BriefingRoom library.
+    /// </summary>
+    public sealed class BriefingRoomLibrary : IDisposable
+    {
+        public BriefingRoomLibrary()
+        {
+            Database.Instance.Initialize();
+        }
+
+        public DCSMission GenerateMission(MissionTemplate template)
+        {
+            return null;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ Source/BriefingRoom/DB/DBEntryObjectiveBehavior.cs
@@ -0,0 +1,47 @@
+﻿/*
+==========================================================================
+This file is part of Briefing Room for DCS World, a mission
+generator for DCS World, by @akaAgar (https://github.com/akaAgar/briefing-room-for-dcs)
+
+Briefing Room for DCS World is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+Briefing Room for DCS World is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Briefing Room for DCS World. If not, see https://www.gnu.org/licenses/
+==========================================================================
+*/
+
+using BriefingRoom.Debug;
+using System.IO;
+
+namespace BriefingRoom.DB
+{
+    /// <summary>
+    /// Stores information about the behavior of an objective target.
+    /// </summary>
+    public class DBEntryObjectiveBehavior : DBEntry
+    {
+        /// <summary>
+        /// Loads a database entry from an .ini file.
+        /// </summary>
+        /// <param name="iniFilePath">Path to the .ini file where entry inforation is stored</param>
+        /// <returns>True is successful, false if an error happened</returns>
+
+        protected override bool OnLoad(string iniFilePath)
+        {
+            using (INIFile ini = new INIFile(iniFilePath))
+            {
+
+            }
+
+            return true;
+        }
+    }
+}
