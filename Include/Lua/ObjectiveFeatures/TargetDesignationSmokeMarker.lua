f1b18da604265de2dfd4f9b6649c99c4873e8eab
Agar <47314805+akaAgar@users.noreply.github.com>
2021-06-04 14:42:10 +0200

    Added smoke marker objective feature

--- Include/Lua/ObjectiveFeatures/TargetDesignationSmokeMarker.lua
+++ Include/Lua/ObjectiveFeatures/TargetDesignationSmokeMarker.lua
@@ -1,25 +1,45 @@
--- [GUI]
--- DisplayName=Smoke marker on target
--- Category=Target designation
--- Description=Adds an F10 menu command to mark the target with green (if it is an ally) or red (if it is an enemy) smoke.
+-- Time (in seconds) until next smoke is available
+briefingRoom.mission.objectiveFeatures[$OBJECTIVEINDEX$].targetDesignationSmokeMarkerNextSmoke = -999999
 
--- [Feature]
--- IncompatiblePrefix=
+-- Spawn smoke marker
+briefingRoom.mission.objectiveFeatures[$OBJECTIVEINDEX$].targetDesignationSmokeMarkerDoSmoke = function(args)
+  trigger.action.smoke(args.position, args.color)
+  return nil
+end
 
--- [Briefing]
--- Remarks=Friendly units at objective $OBJECTIVENAME$ can mark their position with smoke.
--- Remarks.Enemy=JTAC units can mark targets at objective $OBJECTIVENAME$ with smoke.
+-- "Mark target with smoke" F10 radio command
+briefingRoom.mission.objectiveFeatures[$OBJECTIVEINDEX$].targetDesignationSmokeMarker = function()
+  briefingRoom.radioManager.play("I have no visual. Pop a smoke grenade on target.", "RadioPilotMarkTargetWithSmoke")
 
--- [Include]
--- Lua=TargetDesignationSmokeMarker
--- LuaSettings=
--- Ogg=
-
--- [UnitGroup]
--- Families=
--- Flags=
--- Lua.Group=
--- Lua.Unit=
--- Size=
--- SpawnDistance=
--- ValidSpawnPoints=
+  if #briefingRoom.mission.objectives[$OBJECTIVEINDEX$].unitsID == 0 then -- no target units left
+    briefingRoom.radioManager.play("Negative, no visual on any target.", "RadioSupportNoTarget", briefingRoom.radioManager.getAnswerDelay())
+    return
+  end
+  
+  local unit = dcsExtensions.getUnitByID(briefingRoom.mission.objectives[$OBJECTIVEINDEX$].unitsID[1])
+  if unit == nil then -- no unit found with the ID, try searching for a static
+    unit = dcsExtensions.getStaticByID(briefingRoom.mission.objectives[$OBJECTIVEINDEX$].unitsID[1])
+    if unit == nil then -- no unit nor static found with the ID
+      briefingRoom.radioManager.play("Negative, no visual on any target.", "RadioSupportNoTarget", briefingRoom.radioManager.getAnswerDelay())
+      return
+    end
+  end
+  
+  local timeNow = timer.getAbsTime()
+  
+  if timeNow < briefingRoom.mission.objectiveFeatures[$OBJECTIVEINDEX$].targetDesignationSmokeMarkerNextSmoke then -- Smoke not ready
+    briefingRoom.radioManager.play("Target is already marked with smoke. Check your position, you should see it.", "RadioSupportTargetAlreadyMarkedWithSmoke", briefingRoom.radioManager.getAnswerDelay())
+    return
+  end
+  
+  -- Set cooldown for next smoke
+  briefingRoom.mission.objectiveFeatures[$OBJECTIVEINDEX$].targetDesignationSmokeMarkerNextSmoke = timeNow + SMOKE_DURATION
+  
+  -- Play radio message and setup smoke creating function
+  local args = { position = unit:getPoint(), color = trigger.smokeColor.Red }
+  if unit:getCoalition() == $LUAPLAYERCOALITION$ then color = trigger.smokeColor.Green end
+  briefingRoom.radioManager.play("Affirm, target marked with smoke.", "RadioSupportTargetMarkedWithSmoke", briefingRoom.radioManager.getAnswerDelay(), briefingRoom.mission.objectiveFeatures[$OBJECTIVEINDEX$].targetDesignationSmokeMarkerDoSmoke, args)
+end
+      
+-- Add the command to the F10 menu
+missionCommands.addCommandForCoalition($LUAPLAYERCOALITION$, "Mark target with smoke", briefingRoom.f10Menu.objectives[$OBJECTIVEINDEX$], briefingRoom.mission.objectiveFeatures[$OBJECTIVEINDEX$].targetDesignationSmokeMarker)
