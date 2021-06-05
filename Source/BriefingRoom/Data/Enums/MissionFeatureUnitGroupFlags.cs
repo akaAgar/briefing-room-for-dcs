6cbef7026870f516e0993f9aa160a023e528484c
Agar <47314805+akaAgar@users.noreply.github.com>
2021-06-05 09:29:49 +0200

    Completed DBEntryObjectiveTargetBehavior

--- Source/BriefingRoom/Data/Enums/MissionFeatureUnitGroupFlags.cs
+++ Source/BriefingRoom/Data/Enums/MissionFeatureUnitGroupFlags.cs
@@ -34,64 +34,37 @@ namespace BriefingRoom4DCS.Data
         /// Unit is always shown on map, no matter the value of <see cref="Template.MissionTemplate.OptionsShowEnemyUnits"/>.
         /// </summary>
         AlwaysOnMap = 1,
+
         /// <summary>
         /// Short-range (radar, IR and AAA) should be "embedded" in this group to provide cover.
         /// Exact amount of air defense depends on the <see cref="Template.MissionTemplate.SituationEnemyAirDefense"/> setting.
         /// Only valid for groups of category <see cref="UnitCategory.Vehicle"/>.
         /// </summary>
         EmbeddedAirDefense = 2,
+
         /// <summary>
         /// Unit belongs to the friendly coalition (default is enemy).
         /// </summary>
         Friendly = 4,
+
         /// <summary>
         /// Unit is never shown on map, no matter the value of <see cref="Template.MissionTemplate.OptionsShowEnemyUnits"/>.
         /// </summary>
         NeverOnMap = 8,
 
-        /// <summary>
-        /// This group is not a target
-        /// </summary>
-        NotObjectiveTarget = 16,
+        ///// <summary>
+        ///// This unit group requires a TACAN frequency to be generated
+        ///// </summary>
+        TACAN = 16,
 
-        /// <summary>
-        /// force delay of spawn till after takeoff
-        /// </summary>
-        DelaySpawn = 32,
+        ///// <summary>
+        ///// This unit group must be spawned away from the mission area
+        ///// </summary>
+        AwayFromMissionArea = 32,
 
-        /// <summary>
-        /// Sets waypoint to objective
-        /// </summary>
-        DestinationObjective = 64,
-
-        /// <summary>
-        /// Sets waypoint to player airbase
-        /// </summary>
-        DestinationPlayerBase = 128,
-
-        /// <summary>
-        /// Parked Aircraft should avoid hardened bunkers
-        /// </summary>
-        AvoidHardenedBunkers = 256,
-
-        /// <summary>
-        /// Aircraft should be manually activated by a script, and not added to the queue of aircraft groups to activate
-        /// </summary>
-        ManualActivation = 512,
-
-        /// <summary>
-        /// This unit group requires a TACAN frequency to be generated
-        /// </summary>
-        TACAN = 1024,
-
-        /// <summary>
-        /// This unit group must be spawned away from the mission area
-        /// </summary>
-        AwayFromMissionArea = 2048,
-
-        /// <summary>
-        /// Aircraft should be immediately activated when the first player takes off.
-        /// </summary>
-        ImmediateAircraftActivation = 4096
+        ///// <summary>
+        ///// Aircraft should be immediately activated when the first player takes off.
+        ///// </summary>
+        ImmediateAircraftActivation = 64
     }
 }
