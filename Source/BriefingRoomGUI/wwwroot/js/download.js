9ccc1212e2b521b673e0375d0f4562816ed2cdac
john681611 <john681611@hotmail.com>
2021-06-11 19:13:30 +0100

    add in new download method

--- Source/BriefingRoomGUI/wwwroot/js/download.js
+++ Source/BriefingRoomGUI/wwwroot/js/download.js
@@ -1,18 +1,36 @@
-function BlazorDownloadFile(filename, contentType, content) {
+async function BlazorDownloadFile(filename, contentType, content) {
     // Blazor marshall byte[] to a base64 string, so we first need to convert the string (content) to a Uint8Array to create the File
     const data = base64DecToArr(content);
 
     // Create the URL
     const file = new File([data], filename, { type: contentType });
     const exportUrl = URL.createObjectURL(file);
+    if(self.showSaveFilePicker) {
+      const fileHandle = await self.showSaveFilePicker({
+        suggestedName: filename,
+        types: [{
+          description: 'Text documents',
+          accept: {
+            'text/plain': ['.miz'],
+          },
+        }],
+      });
 
-    // Create the <a> element and click on it
-    const a = document.createElement("a");
-    document.body.appendChild(a);
-    a.href = exportUrl;
-    a.download = filename;
-    a.target = "_self";
-    a.click();
+      const writable = await fileHandle.createWritable();
+      // Write the contents of the file to the stream.
+      await writable.write(file);
+      // Close the file and write the contents to disk.
+      await writable.close();
+    } else {
+
+      // // Create the <a> element and click on it
+      const a = document.createElement("a");
+      document.body.appendChild(a);
+      a.href = exportUrl;
+      a.download = filename;
+      a.target = "_self";
+      a.click();
+    }
 
     // We don't need to keep the url, let's release the memory
     // On Safari it seems you need to comment this line... (please let me know if you know why)
