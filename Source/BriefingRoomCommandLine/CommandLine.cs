75b7851f0b9402bcc7a5baa0bb87627c024c9f02
Agar <47314805+akaAgar@users.noreply.github.com>
2021-06-28 16:15:02 +0200

    Simplified command-line syntax, added message for when command-line is invalid.

--- Source/BriefingRoomCommandLine/CommandLine.cs
+++ Source/BriefingRoomCommandLine/CommandLine.cs
@@ -20,8 +20,8 @@ along with Briefing Room for DCS World. If not, see https://www.gnu.org/licenses
 
 using BriefingRoom4DCS.Mission;
 using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace BriefingRoom4DCS.CommandLineTool
@@ -31,11 +31,6 @@ namespace BriefingRoom4DCS.CommandLineTool
     /// </summary>
     public class CommandLine : IDisposable
     {
-        /// <summary>
-        /// Maximum number of missions to generate.
-        /// </summary>
-        private const int MAX_MISSION_COUNT = 10;
-
         /// <summary>
         /// Path to the log file where BriefingRoom output must be written.
         /// </summary>
@@ -84,6 +79,11 @@ namespace BriefingRoom4DCS.CommandLineTool
             BriefingRoomGenerator = new BriefingRoom(WriteToDebugLog);
         }
 
+        /// <summary>
+        /// Debug log writer method.
+        /// </summary>
+        /// <param name="message">Message to write.</param>
+        /// <param name="errorLevel">Error level (info, warning, error).</param>
         private void WriteToDebugLog(string message, LogMessageErrorLevel errorLevel = LogMessageErrorLevel.Info)
         {
             switch (errorLevel)
@@ -103,40 +103,43 @@ namespace BriefingRoom4DCS.CommandLineTool
         /// <returns>True if everything when right, false otherwise.</returns>
         public bool DoCommandLine(string[] args)
         {
-            ParseCommandLineArguments(args, out string[] templateFiles, out int missionCount);
+            string[] templateFiles = (from string arg in args where File.Exists(arg) select arg).ToArray();
+            string[] invalidTemplateFiles = (from string arg in args where !File.Exists(arg) select arg).ToArray();
+
+            foreach (string filePath in invalidTemplateFiles)
+                WriteToDebugLog($"Template file {filePath} doesn't exist.", LogMessageErrorLevel.Warning);
 
             if (templateFiles.Length == 0)
             {
-                WriteToDebugLog("No valid mission template files given as parameters.", LogMessageErrorLevel.Warning);
+                WriteToDebugLog("No valid mission template files given as parameters.", LogMessageErrorLevel.Error);
+                WriteToDebugLog("");
+                WriteToDebugLog("Command-line format is BriefingRoomCommandLine.exe <MissionTemplate.brt> [<MissionTemplate2.brt> <MissionTemplate3.brt>...]");
                 return false;
             }
 
             foreach (string t in templateFiles)
             {
-                for (int i = 0; i < missionCount; i++)
+                DCSMission mission = BriefingRoomGenerator.GenerateMission(t);
+                if (mission == null)
                 {
-                    DCSMission mission = BriefingRoomGenerator.GenerateMission(t);
-                    if (mission == null)
-                    {
-                        Console.WriteLine($"Failed to generate a mission from template {Path.GetFileName(t)}");
-                        continue;
-                    }
-
-                    string mizFileName;
-                    if (templateFiles.Length == 1) // Single template file provided, use the mission name as file name.
-                        mizFileName = Path.Combine(Application.StartupPath, RemoveInvalidPathCharacters(mission.Briefing.Name) + ".miz");
-                    else // Multiple template files provided, use the template name as file name so we know from which template mission was generated.
-                        mizFileName = Path.Combine(Application.StartupPath, Path.GetFileNameWithoutExtension(t) + ".miz");
-                    mizFileName = GetUnusedFileName(mizFileName);
-
-                    if (!mission.SaveToMizFile(mizFileName))
-                    {
-                        WriteToDebugLog($"Failed to export .miz file from template {Path.GetFileName(t)}", LogMessageErrorLevel.Warning);
-                        continue;
-                    }
-                    else
-                        WriteToDebugLog($"Mission {Path.GetFileName(mizFileName)} exported to .miz file from template {Path.GetFileName(t)}");
+                    Console.WriteLine($"Failed to generate a mission from template {Path.GetFileName(t)}");
+                    continue;
                 }
+
+                string mizFileName;
+                if (templateFiles.Length == 1) // Single template file provided, use the mission name as file name.
+                    mizFileName = Path.Combine(Application.StartupPath, RemoveInvalidPathCharacters(mission.Briefing.Name) + ".miz");
+                else // Multiple template files provided, use the template name as file name so we know from which template mission was generated.
+                    mizFileName = Path.Combine(Application.StartupPath, Path.GetFileNameWithoutExtension(t) + ".miz");
+                mizFileName = GetUnusedFileName(mizFileName);
+
+                if (!mission.SaveToMizFile(mizFileName))
+                {
+                    WriteToDebugLog($"Failed to export .miz file from template {Path.GetFileName(t)}", LogMessageErrorLevel.Warning);
+                    continue;
+                }
+                else
+                    WriteToDebugLog($"Mission {Path.GetFileName(mizFileName)} exported to .miz file from template {Path.GetFileName(t)}");
             }
 
             return true;
@@ -151,33 +154,6 @@ namespace BriefingRoom4DCS.CommandLineTool
         {
             if (string.IsNullOrEmpty(fileName)) return "_";
             return string.Join("_", fileName.Split(Path.GetInvalidFileNameChars()));
-        }
-
-        /// <summary>
-        /// Parse command line arguments to find if they're valid .brt files.
-        /// </summary>
-        /// <param name="args">Command-line arguments.</param>
-        /// <param name="templates">Valid mission templates.</param>
-        /// <param name="missionCount">Number of missions to generate.</param>
-        private void ParseCommandLineArguments(string[] args, out string[] templates, out int missionCount)
-        {
-            missionCount = 0;
-            List<string> templateFiles = new List<string>();
-
-            // Parse all arguments to try to know
-            foreach (string arg in args)
-            {
-                // Argument is an integer, use it as the number of missions to generate
-                if (int.TryParse(arg, out int argAsInt))
-                    missionCount = argAsInt;
-
-                // Argument is a template file, add it to the list of template files to parse
-                if (File.Exists(arg) && arg.ToLowerInvariant().EndsWith(".brt"))
-                    templateFiles.Add(Path.GetFullPath(arg));
-            }
-
-            missionCount = Toolbox.Clamp(missionCount, 1, MAX_MISSION_COUNT);
-            templates = templateFiles.ToArray();
         }
 
         /// <summary>
