00b2260294b3bdfa5dd564a13af15b0ba5c975e6
john681611 <john681611@hotmail.com>
2021-07-03 15:29:56 +0100

    more Syria Bases

--- dataExtractors/extractAirFieldPositions.py
+++ dataExtractors/extractAirFieldPositions.py
@@ -1,11 +1,11 @@
 # Fill all positions on chosen airfield with aircraft (preferably same type)
 # Extract Missions file to temp.txt
-# Edit extracted data so that parking information is formatted as so ["parking_id"] = "17",-508.40157664646,233.57108844941, (VS code helps here)
+# Edit extracted data so that parking information is formatted as so ["parking"] = "17",-508.40157664646,233.57108844941, (VS code helps here)
 # Run Script
 
 f = open("temp.txt", "r")
 data = f.read();
-positions = [f.strip().replace('["parking_id"] = ', "").replace('"', "") for f in data.split("\n") if "parking_id" in f]
+positions = [f.strip().replace('["parking"] = ', "").replace('"', "") for f in data.split("\n") if 'parking"' in f]
 for pos in positions:
     posBits = pos.split(",")
     Id = int(posBits[0])
