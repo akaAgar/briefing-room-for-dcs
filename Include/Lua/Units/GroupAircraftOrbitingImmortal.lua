43510563a1b943c4cfd914727b2dafb34ecdbf10
john681611 <john681611@hotmail.com>
2021-11-24 01:19:08 +0000

    Add Scenery Drone and JTAC as objective features

---
+++ Include/Lua/Units/GroupAircraftOrbitingImmortal.lua
@@ -0,0 +1,83 @@
+  ["lateActivation"] = true,
+  ["tasks"] =
+  {
+  }, -- end of ["tasks"]
+  ["task"] = "CAP",
+  ["uncontrolled"] = false,
+  ["taskSelected"] = true,
+  ["route"] =
+  {
+    ["routeRelativeTOT"] = true,
+    ["points"] =
+    {
+      [1] =
+      {
+        ["alt"] = $ALTITUDE$,
+        ["action"] = "Turning Point",
+        ["alt_type"] = "BARO",
+        ["speed"] = $SPEED$,
+        ["task"] =
+        {
+          ["id"] = "ComboTask",
+          ["params"] =
+          {
+            ["tasks"] =
+            {
+              [1] =
+              {
+                ["enabled"] = true,
+                ["auto"] = false,
+                ["id"] = "Orbit",
+                ["number"] = 1,
+                ["params"] =
+                {
+                  ["altitude"] = $ALTITUDE$,
+                  ["pattern"] = "Circle",
+                  ["speed"] = $SPEED$,
+                }, -- end of ["params"]
+              }, -- end of [1]
+              [2] = 
+              {
+                  ["enabled"] = true,
+                  ["auto"] = false,
+                  ["id"] = "WrappedAction",
+                  ["number"] = 3,
+                  ["params"] = 
+                  {
+                      ["action"] = 
+                      {
+                          ["id"] = "SetInvisible",
+                          ["params"] = 
+                          {
+                              ["value"] = true,
+                          }, -- end of ["params"]
+                      }, -- end of ["action"]
+                  }, -- end of ["params"]
+              }, -- end of [2]
+            }, -- end of ["tasks"]
+          }, -- end of ["params"]
+        }, -- end of ["task"]
+        ["type"] = "Turning Point",
+        ["ETA"] = 0,
+        ["ETA_locked"] = true,
+        ["y"] = $GROUPY$,
+        ["x"] = $GROUPX$,
+        ["name"] = "",
+        ["formation_template"] = "",
+        ["speed_locked"] = true,
+      }, -- end of [1]
+    }, -- end of ["points"]
+  }, -- end of ["route"]
+  ["groupId"] = $GROUPID$,
+  ["hidden"] = $HIDDEN$,
+  ["units"] =
+  {
+$UNITS$
+  }, -- end of ["units"]
+  ["y"] = $GROUPY$,
+  ["x"] = $GROUPX$,
+  ["name"] = "$NAME$",
+  ["communication"] = true,
+  ["start_time"] = 0,
+  ["modulation"] = $RADIOBAND$,
+  ["frequency"] = $RADIOFREQUENCY$,
