1a0bab7857190d2a2b667905d65a4ebab56ecc33
john681611 <john681611@hotmail.com>
2022-06-13 17:57:37 +0100

    Fix: Aircraft & Ship SubTask Spread

--- Source/BriefingRoom/Generator/UnitMakerSpawnPointSelector.cs
+++ Source/BriefingRoom/Generator/UnitMakerSpawnPointSelector.cs
@@ -99,7 +99,7 @@ namespace BriefingRoom4DCS.Generator
             Coordinates origin)
         {
             if (validTypes.Contains(SpawnPointType.Air) || validTypes.Contains(SpawnPointType.Sea))
-                return Coordinates.CreateRandom(origin, new MinMaxD(1, 3));
+                return Coordinates.CreateRandom(origin, new MinMaxD(1 * Toolbox.NM_TO_METERS, 3 * Toolbox.NM_TO_METERS));
             var sp = SpawnPoints.Where(x => validTypes.Contains(x.PointType)).Aggregate((acc, x) => origin.GetDistanceFrom(x.Coordinates) < origin.GetDistanceFrom(acc.Coordinates) ? x : acc);
             SpawnPoints.Remove(sp);
             return sp.Coordinates;
