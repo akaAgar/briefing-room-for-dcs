1eee56aac0813a6a556d108fca5265134fa5f5fc
john681611 <john681611@hotmail.com>
2022-08-13 21:32:24 +0100

    Fix: Template can now survive ED's editing

--- Source/BriefingRoom/Library/MizMaker.cs
+++ Source/BriefingRoom/Library/MizMaker.cs
@@ -38,8 +38,10 @@ namespace BriefingRoom4DCS
 
             AddStringValueToEntries(MizFileEntries, "briefing.html", mission.Briefing.GetBriefingAsHTML(true));
             AddStringValueToEntries(MizFileEntries, "credits.txt", $"Generated with BriefingRoom for DCS World (https://akaagar.itch.io/briefing-room-for-dcs) {BriefingRoom.VERSION} ({BriefingRoom.BUILD_VERSION})");
-            if (template != null)
-                AddStringValueToEntries(MizFileEntries, "template.brt", Encoding.ASCII.GetString(template.GetIniBytes()));
+            if (template != null){
+                AddStringValueToEntries(MizFileEntries, "l10n/DEFAULT/template.brt", Encoding.ASCII.GetString(template.GetIniBytes()));
+                mission.AppendValue("MapResourcesFiles", $"[\"ResKey_Snd_template\"] = \"template.brt\",\n");
+            }
             AddLuaFileToEntries(MizFileEntries, "mission", "Mission.lua", mission);
             AddLuaFileToEntries(MizFileEntries, "options", "Options.lua", null);
             AddStringValueToEntries(MizFileEntries, "theatre", mission.GetValue("TheaterID"));
