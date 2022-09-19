961769d7c6a2e8fa464ce2b580df62ee2adab4c9
john681611 <john681611@hotmail.com>
2022-09-19 12:02:42 +0100

    Updated: Migrate Spawn anywhere to Situation option

--- Source/BriefingRoom/Generator/MissionGenerator.cs
+++ Source/BriefingRoom/Generator/MissionGenerator.cs
@@ -55,6 +55,8 @@ namespace BriefingRoom4DCS.Generator
                 );
             if (template.ContextSituation.StartsWith(template.ContextTheater))
                 situationDB = Database.Instance.GetEntry<DBEntrySituation>(template.ContextSituation);
+            else if (template.ContextSituation == "None")
+                template.OptionsMission.Add("SpawnAnywhere");
 
 
             var coalitionsDB = new DBEntryCoalition[]
@@ -65,7 +67,7 @@ namespace BriefingRoom4DCS.Generator
 
             // Copy values from the template
             mission.SetValue("BriefingTheater", theaterDB.UIDisplayName.Get());
-            mission.SetValue("BriefingSituation", situationDB.UIDisplayName.Get());
+            mission.SetValue("BriefingSituation", template.ContextSituation == "None" ? "" : situationDB.UIDisplayName.Get());
             mission.SetValue("BriefingAllyCoalition", coalitionsDB[(int)template.ContextPlayerCoalition].UIDisplayName.Get());
             mission.SetValue("BriefingEnemyCoalition", coalitionsDB[(int)template.ContextPlayerCoalition.GetEnemy()].UIDisplayName.Get());
             mission.SetValue("EnableAudioRadioMessages", !template.OptionsMission.Contains("RadioMessagesTextOnly"));
