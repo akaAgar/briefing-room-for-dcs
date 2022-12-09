b66b707cdd14df362e32bee55c23836f5daf040f
john681611 <john681611@hotmail.com>
2022-12-09 18:52:59 +0000

    Revert "Updated: CSAR script to Moose version"
    This reverts commit b0096ac407e5c06f8c79ae5530260ba38135bdf0.

--- Include/Lua/MissionFeatures/Moose_CSAR.lua
+++
@@ -1,7 +0,0 @@
-   -- Instantiate and start a CSAR for the blue side, with template "Downed Pilot" and alias "Luftrettung"
-   local my_csar = CSAR:New(briefingRoom.playerCoalition,"Downed Pilot","Downed Pilot")
-   -- options
-   my_csar.immortalcrew = true -- downed pilot spawn is immortal
-   my_csar.invisiblecrew = false -- downed pilot spawn is visible
-   -- start the FSM
-   my_csar:__Start(5)
