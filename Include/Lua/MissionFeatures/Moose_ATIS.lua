299bdb37902dc9b0d2adca0a2ae3e574418add9b
john681611 <john681611@hotmail.com>
2023-01-10 19:57:56 +0000

    Fix: Moose ATIS wasn't working

--- Include/Lua/MissionFeatures/Moose_ATIS.lua
+++ Include/Lua/MissionFeatures/Moose_ATIS.lua
@@ -1,3 +1,3 @@
 atisBatumi = ATIS:New("$PLAYERAIRBASENAME$", 227.00)
-atisBatumi:SetRadioRelayUnitName(briefingRoom.mission.missionFeatures.unitNames.moose_ATIS[1])
+atisBatumi:SetRadioRelayUnitName(briefingRoom.mission.missionFeatures.unitNames.aTIS[1])
 atisBatumi:Start()
