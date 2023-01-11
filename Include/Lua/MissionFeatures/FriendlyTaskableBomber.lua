5a7d7ba85ebc256abc1b66ca978fdbada15fc7c4
john681611 <john681611@hotmail.com>
2023-01-11 18:36:27 +0000

    Fix: Friendly Taskable Bomber not bombing  - Thanks Eagle01

--- Include/Lua/MissionFeatures/FriendlyTaskableBomber.lua
+++ Include/Lua/MissionFeatures/FriendlyTaskableBomber.lua
@@ -21,8 +21,29 @@ function briefingRoom.mission.missionFeatures.friendlyTaskableBomber.launchBombi
       params = {
         route = {
           points = {
-            [1] = {
-              speed = 200,
+			[1] = {
+              speed = 260,
+              x = dcsExtensions.lerp(currPos.x, mark.pos.x, 0.3),
+              y = dcsExtensions.lerp(currPos.z, mark.pos.z, 0.3),
+              type = 'Turning Point',
+              ETA_locked = false,
+              ETA = 100,
+              alt = 7620,
+              alt_type = "BARO",
+              speed_locked = false,
+              action = "Turning Point",
+              name = "INGRESS",
+              task = {
+                id = "ComboTask",
+                params = {
+                  tasks = {
+                    
+                  },
+                },
+              },
+            },
+            [2] = {
+              speed = 320,
               x = dcsExtensions.lerp(currPos.x, mark.pos.x, 0.7),
               y = dcsExtensions.lerp(currPos.z, mark.pos.z, 0.7),
               type = 'Turning Point',
@@ -31,7 +52,7 @@ function briefingRoom.mission.missionFeatures.friendlyTaskableBomber.launchBombi
               alt = 7620,
               alt_type = "BARO",
               speed_locked = false,
-              action = "Fly Over Point",
+              action = "Turning Point",
               name = "BOMB",
               task = {
                 id = "ComboTask",
@@ -44,32 +65,52 @@ function briefingRoom.mission.missionFeatures.friendlyTaskableBomber.launchBombi
                       number = 1,
                       params = {
                         direction = 0,
-                        attackQtyLimit = true,
+                        attackQtyLimit = false,
                         attackQty = 1,
                         expend = "Auto",
                         directionEnabled = false,
                         groupAttack = true,
                         altitude = 2000,
                         altitudeEnabled = false,
-                        weaponType = 2147485694,
+                        weaponType = 9663676414,
                         y = mark.pos.z,
                         x = mark.pos.x,
                       },
                     },
-                    [2] = {
+                  },
+                },
+              },
+            },
+			[3] = {
+              speed = 200,
+              x = dcsExtensions.lerp(currPos.x, mark.pos.x, 0.5),
+              y = dcsExtensions.lerp(currPos.z, mark.pos.z, 0.5),
+              type = 'Turning Point',
+              ETA_locked = false,
+              ETA = 100,
+              alt = 7620,
+              alt_type = "BARO",
+              speed_locked = false,
+              action = "Turning Point",
+              name = "ORBIT",
+              task = {
+                id = "ComboTask",
+                params = {
+                  tasks = {
+                    [1] = {
                       enabled = true,
                       auto = false,
                       id = "Orbit",
-                      number = 2,
+                      number = 1,
                       params = {
                         pattern = "Circle",
-                      }
-                    }
-                  }
-                }
-              }
+                      },
+                    },
+                  },
+                },
+              },
             },
-          }
+          },
         },
       },
     }
