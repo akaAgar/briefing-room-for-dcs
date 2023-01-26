c067bfad0465891fcc1c31289d32a09a6fcc2ce0
Gabriel Vázquez <gavazquez@gmail.com>
2023-01-26 18:55:30 +0100

    Made project as MAUI

---
+++ Source/BriefingRoomDesktop/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BriefingRoom4DCS.GUI.Desktop
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
