4f508de6b68c9a9390dd89d3a8c9765b699c88c6
john681611 <john681611@hotmail.com>
2023-07-22 20:51:34 +0100

    Updated: SituationEditor JSON usage

--- Source/BriefingRoom/Data/JSON/ParseClasses/Situation.cs
+++ Source/BriefingRoom/Data/JSON/ParseClasses/Situation.cs
@@ -5,10 +5,10 @@ namespace BriefingRoom4DCS.Data.JSON
 {
     public class Situation
     {   
-        public Dictionary<string,string> DisplayName { get; set; }
+        public Dictionary<string,string> DisplayName { get; set; } = new Dictionary<string,string>{{"EN", "<NAME HERE>"}};
         public string Theater { get; set; }
-        public List<Dictionary<string,string>> BriefingDescriptions { get; set; }
-        public List<string> RelatedSituations { get; set; }
+        public List<Dictionary<string,string>> BriefingDescriptions { get; set; } = new List<Dictionary<string,string>>();
+        public List<string> RelatedSituations { get; set; } = new List<string>();
         public List<List<List<double>>> redZones { get; set; }
         public List<List<List<double>>> blueZones { get; set; }
         public List<List<List<double>>> noSpawnZones { get; set; }
