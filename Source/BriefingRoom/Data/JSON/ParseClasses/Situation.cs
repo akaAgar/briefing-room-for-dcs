aa4fb65b737c5fddbdf35684145525f3f3f07052
john681611 <john681611@hotmail.com>
2023-09-20 09:14:55 +0100

    Added: Inital German Translation Support

--- Source/BriefingRoom/Data/JSON/ParseClasses/Situation.cs
+++ Source/BriefingRoom/Data/JSON/ParseClasses/Situation.cs
@@ -5,7 +5,7 @@ namespace BriefingRoom4DCS.Data.JSON
 {
     public class Situation
     {   
-        public Dictionary<string,string> DisplayName { get; set; } = new Dictionary<string,string>{{"EN", "<NAME HERE>"}};
+        public Dictionary<string,string> DisplayName { get; set; } = new Dictionary<string,string>{{"en", "<NAME HERE>"}};
         public string Theater { get; set; }
         public List<Dictionary<string,string>> BriefingDescriptions { get; set; } = new List<Dictionary<string,string>>();
         public List<string> RelatedSituations { get; set; } = new List<string>();
