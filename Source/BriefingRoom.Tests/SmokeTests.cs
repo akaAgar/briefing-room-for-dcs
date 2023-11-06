89baa026cf41b75c7fd586c994e295a9881ab0cc
john681611 <john681611@hotmail.com>
2023-11-06 15:55:52 +0000

    Testing: Added smoke tests

---
+++ Source/BriefingRoom.Tests/SmokeTests.cs
@@ -0,0 +1,36 @@
+using BriefingRoom4DCS.Mission;
+
+namespace BriefingRoom4DCS.Tests;
+
+public class SmokeTests
+{
+    [Fact]
+    async public void SingleMission()
+    {
+        new BriefingRoom();
+        DCSMission mission = await BriefingRoom.GenerateMissionAsync($"{BriefingRoom.GetBriefingRoomRootPath()}\\Default.brt");
+
+        Assert.NotNull(mission);
+        Assert.Equal("Caucasus", mission.TheaterID);
+        Assert.NotNull(mission.Briefing.Name);
+        Assert.NotNull(mission.Briefing.Description);
+    }
+
+    [Fact]
+    async public void Campaign()
+    {
+        new BriefingRoom();
+        DCSCampaign campaign = await BriefingRoom.GenerateCampaignAsync($"{BriefingRoom.GetBriefingRoomRootPath()}\\Default.cbrt");
+
+        Assert.NotNull(campaign);
+        Assert.Equal(5, campaign.MissionCount);
+        Assert.NotNull(campaign.Name);
+
+
+        DCSMission mission = campaign.Missions.First();
+        Assert.NotNull(mission);
+        Assert.Equal("Caucasus", mission.TheaterID);
+        Assert.NotNull(mission.Briefing.Name);
+        Assert.NotNull(mission.Briefing.Description);
+    }
+}
