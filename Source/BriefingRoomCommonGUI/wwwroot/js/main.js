872945867447b435e709d6a157f4dd760a9b255e
john681611 <john681611@hotmail.com>
2024-01-04 01:18:39 +0000

    Fix: download failing if taking too long (fall back to simple download)

--- Source/BriefingRoomCommonGUI/wwwroot/js/main.js
+++ Source/BriefingRoomCommonGUI/wwwroot/js/main.js
@@ -1,3 +1,18 @@
+  const simpleDownload = (file, filename) => {
+  // // Create the <a> element and click on it
+  const exportUrl = URL.createObjectURL(file);
+  const a = document.createElement("a");
+  document.body.appendChild(a);
+  a.href = exportUrl;
+  a.download = filename;
+  a.target = "_self";
+  a.click();
+  // We don't need to keep the url, let's release the memory
+  // On Safari it seems you need to comment this line... (please let me know if you know why)
+  URL.revokeObjectURL(exportUrl);
+}
+
+
 async function BlazorDownloadFile(filename, contentType, data) {
 
   
@@ -14,34 +29,33 @@ async function BlazorDownloadFile(filename, contentType, data) {
     return
   }
   if (self.showSaveFilePicker) {
-    const fileHandle = await self.showSaveFilePicker({
-      suggestedName: filename,
-      types: [{
-        description: 'Text documents',
-        accept: {
-          'text/plain': [`.${fileType}`],
-        },
-      }],
-    });
+    try {
+      const fileHandle = await self.showSaveFilePicker({
+        suggestedName: filename,
+        types: [{
+          description: 'Text documents',
+          accept: {
+            'text/plain': [`.${fileType}`],
+          },
+        }],
+      });
 
-    const writable = await fileHandle.createWritable();
-    // Write the contents of the file to the stream.
-    await writable.write(file);
-    // Close the file and write the contents to disk.
-    await writable.close();
+      const writable = await fileHandle.createWritable();
+      // Write the contents of the file to the stream.
+      await writable.write(file);
+      // Close the file and write the contents to disk.
+      await writable.close();
+    } catch (error) {
+      if(error.name === "AbortError") {
+        return
+      }
+      console.error(error)
+      simpleDownload(file, filename)
+      return
+    }
+  
   } else {
-
-    // // Create the <a> element and click on it
-    const exportUrl = URL.createObjectURL(file);
-    const a = document.createElement("a");
-    document.body.appendChild(a);
-    a.href = exportUrl;
-    a.download = filename;
-    a.target = "_self";
-    a.click();
-    // We don't need to keep the url, let's release the memory
-    // On Safari it seems you need to comment this line... (please let me know if you know why)
-    URL.revokeObjectURL(exportUrl);
+    simpleDownload(file, filename)
   }
 
 }
