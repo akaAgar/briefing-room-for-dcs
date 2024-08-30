3d89291773a3d53cb53afb6bcd0f44f26063000a
john681611 <john681611@hotmail.com>
2024-08-30 10:25:28 +0100

    Common kill event basics

--- Include/Lua/Script.lua
+++ Include/Lua/Script.lua
@@ -698,48 +698,80 @@ briefingRoom.aircraftActivator.reserveQueueInitialCount = #briefingRoom.aircraft
 briefingRoom.eventHandler = {}
 briefingRoom.eventHandler.BDASetting = "$BDASETTING$"
 
-function briefingRoom.handleGeneralKill(event) 
+function briefingRoom.handleGeneralPlayerKill(event)
+  local playerName = event.initiator:getPlayerName()
+  if playerName == nil then return end
+  briefingRoom.debugPrint("Kill Event: "..event.initiator:getName().." ("..playerName..") killed "..event.target:getName())
+  if event.target:getCoalition() ~= briefingRoom.playerCoalition then -- unit is an enemy, radio some variation of a "enemy destroyed" message
+    local soundName = "UnitDestroyed"
+    local messages = { "$LANG_COMMAND$: $LANG_DESTROY1$", "$LANG_COMMAND$: $LANG_DESTROY2$", "$LANG_COMMAND$: $LANG_SHOOTDOWN1$", "$LANG_COMMAND$: $LANG_SHOOTDOWN2$" }
+    local messageIndex = math.random(1, 2)
+    local messageIndexOffset = 0
+
+
+
+    local targetType = "Ground"
+    if event.id == world.event.S_EVENT_CRASH then
+      messageIndexOffset = 2
+      if event.target:inAir() then
+        targetType = "Air"
+        messageIndexOffset = 2
+      elseif unitWasAMissionTarget then
+        return -- No "target splashed" message when destroying a target aircraft on the ground (mostly for OCA missions)
+      end
+    end
+    if briefingRoom.eventHandler.BDASetting == "ALL" then
+      briefingRoom.radioManager.play(messages[messageIndex + messageIndexOffset], "RadioHQ"..soundName..targetType..tostring(messageIndex), math.random(1, 3))
+    end
+    briefingRoom.aircraftActivator.possibleResponsiveSpawn()
+  else
+    briefingRoom.debugPrint("Friendly Fire Event: "..event.initiator:getName().." ("..playerName..") killed "..event.target:getName())
+  end 
+end
+
+function briefingRoom.handleGeneralPlayerKilled(event)
+  local playerName = event.target:getPlayerName()
+    if playerName == nil then return end
+    briefingRoom.debugPrint("Player Killed Event: "..event.initiator:getName().." killed "..event.target:getName().." ("..playerName..")")
+    briefingRoom.handleTroopsInAircraft(event)
+end
+
+function briefingRoom.handleGeneralKill(event)
+  if event.id == world.event.S_EVENT_KILL then 
+    if event.initiator == nil or event.target == nil or (event.initiator.getPlayerName == nil and event.target.getPlayerName == nil) then return end -- Incomplete event or player not involved
+    if event.initiator.getPlayerName ~= nil then
+      briefingRoom.handleGeneralPlayerKill(event)
+    end
+    if event.target.getPlayerName ~= nil then
+      briefingRoom.handleGeneralPlayerKilled(event)
+    end
+  end
+
+  function briefingRoom.handleTroopsInAircraft(event)
+    local unitName = event.initiator:getName()
+    -- TODO see if we can detect units in aircraft that embarked using DCS radio
+    if table.containsKey(briefingRoom.transportManager.transportRoster, unitName) then
+      local toopNames={}
+      local n=0
+      for k,v in pairs(briefingRoom.transportManager.transportRoster[unitName].troops) do
+        n=n+1
+        toopNames[n]=k
+      end
+      briefingRoom.debugPrint("unpacking troops "..#toopNames)
+      -- Assume all troops are main characters and survive the crash no issues
+      briefingRoom.transportManager.removeTroopCargo(unitName, toopNames, event.initiator:getPoint())
+    end
+    
+  end
+
   if event.id == world.event.S_EVENT_DEAD or event.id == world.event.S_EVENT_CRASH then
     if event.initiator == nil or event.initiator.getCategory == nil then  return end -- no initiator
     briefingRoom.debugPrint("initiator "..event.initiator:getCategory())
     if event.initiator:getCategory() ~= Object.Category.UNIT and event.initiator:getCategory() ~= Object.Category.STATIC then return end -- initiator was not an unit or static
-
-    if event.initiator:getCoalition() ~= briefingRoom.playerCoalition then -- unit is an enemy, radio some variation of a "enemy destroyed" message
-      local soundName = "UnitDestroyed"
-      local messages = { "$LANG_COMMAND$: $LANG_DESTROY1$", "$LANG_COMMAND$: $LANG_DESTROY2$", "$LANG_COMMAND$: $LANG_SHOOTDOWN1$", "$LANG_COMMAND$: $LANG_SHOOTDOWN2$" }
-      local messageIndex = math.random(1, 2)
-      local messageIndexOffset = 0
-
-
-
-      local targetType = "Ground"
-      if event.id == world.event.S_EVENT_CRASH then
-        messageIndexOffset = 2
-        if event.initiator:inAir() then
-          targetType = "Air"
-          messageIndexOffset = 2
-        elseif unitWasAMissionTarget then
-          return -- No "target splashed" message when destroying a target aircraft on the ground (mostly for OCA missions)
-        end
-      end
-      if briefingRoom.eventHandler.BDASetting == "ALL" then
-        briefingRoom.radioManager.play(messages[messageIndex + messageIndexOffset], "RadioHQ"..soundName..targetType..tostring(messageIndex), math.random(1, 3))
-      end
-      briefingRoom.aircraftActivator.possibleResponsiveSpawn()
-    else
+    if event.initiator:getCoalition() == briefingRoom.playerCoalition then
       local unitName = event.initiator:getName()
       briefingRoom.debugPrint("Friendly Crash "..unitName)
-      if table.containsKey(briefingRoom.transportManager.transportRoster, unitName) then
-        local toopNames={}
-        local n=0
-        for k,v in pairs(briefingRoom.transportManager.transportRoster[unitName].troops) do
-          n=n+1
-          toopNames[n]=k
-        end
-        briefingRoom.debugPrint("unpacking troops "..#toopNames)
-        -- Assume all troops are main characters and survive the crash no issues
-        briefingRoom.transportManager.removeTroopCargo(unitName, toopNames, event.initiator:getPoint())
-      end
+      briefingRoom.handleTroopsInAircraft(event)
     end
   end
 end
